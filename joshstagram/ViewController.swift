b895bbfdd30e794079d361855d76afa6214eca22
Name635246 <83032092+Name635246@users.noreply.github.com>
2022-03-21 09:25:21 -0400

    Initial Commit

---
+++ joshstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  joshstagram
+//
+//  Created by Joshua Fordyce on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
