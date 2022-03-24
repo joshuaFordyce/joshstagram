d449814bce23f417bc3b895428039335c4e307dd
Name635246 <83032092+Name635246@users.noreply.github.com>
2022-03-23 21:50:15 -0400

    On branch main  Changes to be committed: 	new file:   joshstagram/Assets.xcassets/App Icon/Contents.json 	new file:   joshstagram/Assets.xcassets/App Icon/Icon-60.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/App Icon/Icon-60.imageset/Icon-60@2x.png 	new file:   joshstagram/Assets.xcassets/App Icon/Icon-60.imageset/Icon-60@3x.png 	new file:   joshstagram/Assets.xcassets/feed_tab.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/feed_tab.imageset/feed_tab.png 	new file:   joshstagram/Assets.xcassets/feed_tab.imageset/feed_tab@2x.png 	new file:   joshstagram/Assets.xcassets/feed_tab.imageset/feed_tab@3x.png 	new file:   joshstagram/Assets.xcassets/image_placeholder.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/image_placeholder.imageset/image_placeholder.png 	new file:   joshstagram/Assets.xcassets/image_placeholder.imageset/image_placeholder@2x.png 	new file:   joshstagram/Assets.xcassets/image_placeholder.imageset/image_placeholder@3x.png 	new file:   joshstagram/Assets.xcassets/insta_camera_btn.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/insta_camera_btn.imageset/insta_camera_btn.png 	new file:   joshstagram/Assets.xcassets/insta_camera_btn.imageset/insta_camera_btn@2x.png 	new file:   joshstagram/Assets.xcassets/insta_camera_btn.imageset/insta_camera_btn@3x.png 	new file:   joshstagram/Assets.xcassets/instagram_logo.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/instagram_logo.imageset/instagram_logo.png 	new file:   joshstagram/Assets.xcassets/profile_tab.imageset/Contents.json 	new file:   joshstagram/Assets.xcassets/profile_tab.imageset/profile_tab.png 	new file:   joshstagram/Assets.xcassets/profile_tab.imageset/profile_tab@2x.png 	new file:   joshstagram/Assets.xcassets/profile_tab.imageset/profile_tab@3x.png 	new file:   joshstagram/CameraViewController.swift 	new file:   joshstagram/FeedViewController.swift 	new file:   joshstagram/LoginViewController.swift 	deleted:    joshstagram/ViewController.swift 	new file:   joshstagram/postTableViewCell.swift
     Changes not staged for commit:
    	modified:   joshstagram.xcodeproj/project.pbxproj
    	modified:   joshstagram.xcodeproj/xcuserdata/joshuafordyce.xcuserdatad/xcschemes/xcschememanagement.plist
    	modified:   joshstagram/AppDelegate.swift
    	modified:   joshstagram/Base.lproj/LaunchScreen.storyboard
    	modified:   joshstagram/Base.lproj/Main.storyboard
    	modified:   joshstagram/CameraViewController.swift
    	modified:   joshstagram/FeedViewController.swift
    	modified:   joshstagram/LoginViewController.swift
    	modified:   joshstagram/postTableViewCell.swift
    
     Untracked files:
    	Podfile
    	Podfile.lock
    	Pods/
    	joshstagram.xcworkspace/

---
+++ joshstagram/postTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  postTableViewCell.swift
+//  joshstagram
+//
+//  Created by Joshua Fordyce on 3/23/22.
+//
+
+import UIKit
+
+class postTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
